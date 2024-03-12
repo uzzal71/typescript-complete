552ea15e733a32c328437a0b4f8ba3c321f23b07
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-12 21:02:17 +0600

    section-1

--- section-1/index.ts
+++ section-1/index.ts
@@ -1,11 +1,19 @@
 import axios from "axios";
 
+interface Todo {
+  ID: number;
+  title: string;
+  completed: boolean;
+}
+
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 axios.get(url).then((response) => {
   const todo = response.data;
+
   const ID = todo.ID;
   const title = todo.title;
   const completed = todo.completed;
+
   console.log(`
     The todo with ID: ${ID}
     Has a title of: ${title}
