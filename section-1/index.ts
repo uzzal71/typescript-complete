81c0ce2026c1eda0dae41fbba7d65e711bbf7b60
uzzal71 <uzzalroy.acm@gmail.com>
2024-03-12 20:56:59 +0600

    section-1

--- section-1/index.ts
+++ section-1/index.ts
@@ -2,5 +2,9 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data;
+  const ID = todo.ID;
+  const title = todo.title;
+  const completed = todo.completed;
+  console.log(`${title}`);
 });
